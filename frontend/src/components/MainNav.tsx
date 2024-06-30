91b7897d37ab7f7794351e8df46c87cec04238bf
Adeleye Abdulbasit Damilola <adeleyeabdulbasitdamilola@gmail.com>
2024-06-30 20:57:29 +0100

    feat: user authetication using Auth0

--- frontend/src/components/MainNav.tsx
+++ frontend/src/components/MainNav.tsx
@@ -1,8 +1,10 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "./ui/button";
 
 const MainNav = () => {
+  const { loginWithRedirect } = useAuth0()
   return (
-    <Button variant="ghost" className="font-bold hover:text-orange-500 hover:bg-white tracking-wide text-lg">
+    <Button onClick={async () => await loginWithRedirect()} variant="ghost" className="font-bold hover:text-orange-500 hover:bg-white tracking-wide text-lg">
       Log In
     </Button>
   )
