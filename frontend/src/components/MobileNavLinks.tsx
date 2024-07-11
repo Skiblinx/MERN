05b5d9301ae8f1e8c84c5ac6bd239972cf00f270
Adeleye Abdulbasit Damilola <adeleyeabdulbasitdamilola@gmail.com>
2024-07-11 11:57:28 +0100

    feat: frontend user model

--- frontend/src/components/MobileNavLinks.tsx
+++ frontend/src/components/MobileNavLinks.tsx
@@ -1,13 +1,18 @@
 import { Link } from "react-router-dom";
 import { Button } from "./ui/button";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Separator } from "./ui/separator";
 
 const MobileNavLinks = () => {
+
+  const { logout } = useAuth0()
   return (
     <>
       <Link to="/user-profile" className="flex bg-white items-center font-bold hover:text-orange-500">
         User Profile
       </Link>
-      <Button className="flex items-center font-bold px-3 hover:bg-gray-500">
+      <Separator />
+      <Button onClick={() => logout()} className="flex items-center font-bold px-3 hover:bg-gray-500">
         Log Out
       </Button>
     </>
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -12,6 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <AppRoutes />
       </Auth0ProviderWithNavigate>
     </Router>
-
   </React.StrictMode>,
 )
+
+
