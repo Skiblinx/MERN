16a2dd85e16b9c8768ab3bbeb1f1afaf68f82132
Adeleye Abdulbasit Damilola <adeleyeabdulbasitdamilola@gmail.com>
2024-07-27 18:19:16 +0100

    feat: user authetication with Auth0

--- frontend/src/auth/AuthProviderWithNavigate.tsx
+++ frontend/src/auth/AuthProviderWithNavigate.tsx
@@ -1,3 +1,4 @@
+import { useCreateMyUSer } from "@/api/MyUserApi";
 import { Auth0Provider, AppState, User } from "@auth0/auth0-react";
 
 type Props = {
@@ -5,6 +6,10 @@ type Props = {
 }
 
 const AuthProviderWithNavigate = ({ children }: Props) => {
+
+  const { createUser } = useCreateMyUSer();
+
+
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH_CALLBACK_URL;
@@ -14,7 +19,9 @@ const AuthProviderWithNavigate = ({ children }: Props) => {
   }
 
   const onRedirectCallback = (appState?: AppState, user?: User) => {
-    console.log("USER: " + user?.name)
+    if (user?.sub && user?.email) {
+      createUser({ auth0Id: user.sub, email: user.email })
+    }
   }
 
   return (
