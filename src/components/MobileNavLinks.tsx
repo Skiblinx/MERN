af102723e771ae8933dbc42f9c83ef82fadd2948
Adeleye Abdulbasit Damilola <adeleyeabdulbasitdamilola@gmail.com>
2024-12-14 21:32:53 +0100

    fix: build error

--- src/components/MobileNavLinks.tsx
+++ src/components/MobileNavLinks.tsx
@@ -12,6 +12,10 @@ const MobileNavLinks = () => {
         User Profile
       </Link>
       <Separator />
+      <Link to="/manage-restuarant" className="flex bg-white items-center font-bold hover:text-orange-500">
+        Manage Restuarant
+      </Link>
+      <Separator />
       <Button onClick={() => logout()} className="flex items-center font-bold px-3 hover:bg-gray-500">
         Log Out
       </Button>
